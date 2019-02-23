93ef41464835996649b0abf3cecedf8b177fffa6
Peter Ho <ostentatiousitus@gmail.com>
2019-02-23 18:34:46 -0500

    move code out of WM_PAINT

--- src/input/windows/wintest.cc
+++ src/input/windows/wintest.cc
@@ -25,16 +25,18 @@ struct tagRectangle
 LRESULT CALLBACK WindowProc(HWND, UINT, WPARAM, LPARAM);
 void refresh_window(void *win);
 
+bool done = false;
+
 int main()
 {
 	srand(time(NULL));
 	
 	Inputs i;
-	
 	rectangles = new tagRectangle;
 	struct tagRectangle *head = rectangles;
 	for (int j = 0; j < i.keyboard_count(); ++j) {
 		head->offsetX = rand() % (GetSystemMetrics(SM_CXSCREEN) / 2 - 50);
+		cout << head->offsetX << endl;
 		head->offsetY = rand() % (GetSystemMetrics(SM_CYSCREEN) / 2 - 50);
 		head->r.left = head->offsetX;
 		head->r.right = head->offsetX + 50;
@@ -50,7 +52,7 @@ int main()
 	
 	_beginthread(create_test_window, 0, NULL);	// create game window
 	
-	while (1) {
+	while (!done) {
 		head = rectangles;
 		while (head) {
 			unsigned short vkey = 0;
@@ -97,17 +99,9 @@ void create_test_window(void *)
 	HWND hwnd;
 	hwnd = FindWindow(NULL, szTitle);
 	
-	// Using callback for timer lags very bad
-	// because it requires 2 interrupts
-	// Use our own timer instead
 	_beginthread(refresh_window, 0, hwnd);
 	
 	while (GetMessage(&msg, NULL, 0, 0)) {
-		// 512 = mouse movement in window
-		// 160 = mouse movement in bar
-		if (msg.message >= WM_KEYFIRST && msg.message <= WM_KEYLAST
-		|| msg.message == 512 || msg.message == 160)
-			continue;
 		TranslateMessage(&msg);
 		DispatchMessage(&msg);
 	}
@@ -117,34 +111,30 @@ void create_test_window(void *)
 
 void refresh_window(void *win)
 {
+	struct tagRectangle *head;
 	HWND hwnd = (HWND)win;
-	while (1) {
-		InvalidateRect(hwnd, NULL, FALSE);
-		Sleep(10);
-	}
-}
-
-LRESULT CALLBACK WindowProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam)
-{	
+	RECT rc;
 	HDC hdc;
 	PAINTSTRUCT ps;
-	RECT rc;
-	struct tagRectangle *head = rectangles;
-	
-	switch(msg) {
-	case WM_PAINT:
+	while (!done) {
+		hdc = GetDC(hwnd);
+		head = rectangles;
 		GetWindowRect(hwnd, &rc);
-		hdc = BeginPaint(hwnd, &ps);
 		FillRect(hdc, &rc, (HBRUSH)(COLOR_WINDOW));
 		while (head) {
 			FillRect(hdc, &(head->r), head->color);
 			head = head->next;
 		}
-		EndPaint(hwnd, &ps);
-		break;
-	case WM_CREATE:
-		break;
+		ReleaseDC(hwnd, hdc);
+		Sleep(10);
+	}
+}
+
+LRESULT CALLBACK WindowProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam)
+{		
+	switch(msg) {
 	case WM_DESTROY:
+		done = 1;
 		PostQuitMessage(0);
 		return 0;
 	}
