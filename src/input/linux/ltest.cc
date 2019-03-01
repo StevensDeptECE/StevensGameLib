eeee1f3e0faf9c4e64535dd83f76a5198a3f3875
Peter Ho <ostentatiousitus@gmail.com>
2019-03-01 10:30:10 -0500

    glfw multi-player (untested)

--- src/input/linux/ltest.cc
+++ src/input/linux/ltest.cc
@@ -1,38 +1,62 @@
 #include "dbkbd.hh"
 #include <time.h>
 #include <pthread.h>
+#include <cstring>
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 
-const char *vertexShaderSource = "#version 330 core\n"
+const char *vertexShaderTemp = "#version 330 core\n"
     "layout (location = 0) in vec3 aPos;\n"
     "void main()\n"
     "{\n"
     "   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
     "}\0";
-const char *fragmentShaderSource = "#version 330 core\n"
+const char *fragmentShaderTemp = "#version 330 core\n"
     "out vec4 FragColor;\n"
     "void main()\n"
     "{\n"
-    "   FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+    "   FragColor = vec4(%ff, %ff, %ff, 1.0f);\n"
     "}\n\0";
-const char *fragmentShader2Source = "#version 330 core\n"
-    "out vec4 FragColor;\n"
-    "void main()\n"
-    "{\n"
-    "   FragColor = vec4(0.2f, 0.5f, 1.0f, 1.0f);\n"
-    "}\n\0";
-
+// "   FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
 
 class Player {
 private:
 	const int size = 125;
 	double r, g, b;
 
+	void make_shader()
+	{
+		char *vertexShaderSource = (char*)malloc(256);
+		char *fragmentShaderSource = (char*)malloc(256);
+		unsigned int vertexShader = glCreateShader(GL_VERTEX_SHADER);
+		unsigned int fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+		strcpy(vertexShaderSource, vertexShaderTemp);
+		sprintf(fragmentShaderSource, fragmentShaderTemp, r, g, b);
+
+		glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+		glCompileShader(vertexShader);
+		glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+		glCompileShader(fragmentShader);
+
+		glAttachShader(shaderProgram, vertexShader);
+		glAttachShader(shaderProgram, fragmentShader);
+		glLinkProgram(shaderProgram);
+
+		glDeleteShader(vertexShader);
+		glDeleteShader(fragmentShader);
+
+		free(vertexShaderSource);
+		free(fragmentShaderSource);
+	}
+
 public:
+	unsigned int shaderProgram;
+	unsigned int VBO, VAO;
+	
 	float x, y;
 	float vertices[12] = {0};
 	int length = 12;
+
 	Player(float a, float b) : x(a), y(b)
 	{
 		r = (double)rand() / RAND_MAX;
@@ -45,12 +69,13 @@ public:
 		vertices[4] = y;
 		vertices[5] = 0.0f;
 		vertices[6] = x+size/800.0;
-		vertices[7] = y+size/600.0;
+		vertices[7] = y-size/600.0;
 		vertices[8] = 0.0f;
 		vertices[9] = x;
-		vertices[10] = y+size/600.0;
+		vertices[10] = y-size/600.0;
 		vertices[11] = 0.0f;
 		length = 12;
+		update();
 	}
 
 	void move(float x, float y)
@@ -62,6 +87,14 @@ public:
 
 	void update()
 	{
+		if (this->x < -1)
+			this->x = -1;
+		if (this->x > 1-size/800.0)
+			this->x = 1-size/800.0;
+		if (this->y > 1)
+			this->y = 1;
+		if (this->y < size/600.0-1)
+			this->y = size/600.0-1;
 		vertices[0] = x;
 		vertices[1] = y;
 		vertices[2] = 0.0f;
@@ -69,32 +102,23 @@ public:
 		vertices[4] = y;
 		vertices[5] = 0.0f;
 		vertices[6] = x+size/800.0;
-		vertices[7] = y+size/600.0;
+		vertices[7] = y-size/600.0;
 		vertices[8] = 0.0f;
 		vertices[9] = x;
-		vertices[10] = y+size/600.0;
+		vertices[10] = y-size/600.0;
 		vertices[11] = 0.0f;
+	}
+
+	void create_shader()
+	{
+		shaderProgram = glCreateProgram();
+		make_shader();
 	}
 };
 
 vector<Player> players;
 bool isIdle = true;
 bool play = true;
-//void draw()
-//{
-//	glClearColor(0.4, 0.4, 0.4, 0.4);
-//	glClear(GL_COLOR_BUFFER_BIT);
-//	for (int i = 0; i < players.size(); ++i)
-//		players[i].draw();
-//}
-
-void idle()
-{
-//	if (!isIdle)
-//		glutPostRedisplay();
-//	isIdle = true;
-	usleep(10000);
-}
 
 void *my_keyboard_handler(void *);
 void drawTriangle();
@@ -104,14 +128,18 @@ int main(int argc, char **argv)
 	srand(time(NULL));
 	Inputs i;
 
-	cout << "Keyboards: " << i.keyboard_count() << endl;
+	// Create players
+	int num_players = i.keyboard_count(); 
+	cout << "Keyboards: " << num_players << endl;
 
+	for (int j = 0; j < num_players; ++j)
+		players.push_back(Player((double)rand() / RAND_MAX - .1, (double)rand() / RAND_MAX - .1));
+
+	// Create keyboard handler thread
 	pthread_t myThread;
 	pthread_create(&myThread, NULL, &my_keyboard_handler, &i);
 
-	for (int j = 0; j < i.keyboard_count(); ++j)
-		players.push_back(Player((double)rand() / RAND_MAX - .1, (double)rand() / RAND_MAX - .1));
-
+	// GLFW Init
 	glfwInit();
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
@@ -124,74 +152,56 @@ int main(int argc, char **argv)
 		return -1;
 	}
 	glfwMakeContextCurrent(window);
-	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 
 	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
 		cout << "Failed init GLAD" << endl;
 		return -1;
 	}
 
-	unsigned int vertexShader = glCreateShader(GL_VERTEX_SHADER);
-	unsigned int fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
-	unsigned int fragmentShader2 = glCreateShader(GL_FRAGMENT_SHADER);
-	unsigned int shaderProgram = glCreateProgram();
-	unsigned int shaderProgram2 = glCreateProgram();
-
-	glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
-	glCompileShader(vertexShader);
-	glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
-	glCompileShader(fragmentShader);
-
-	glShaderSource(fragmentShader2, 1, &fragmentShaderSource, NULL);
-	glCompileShader(fragmentShader2);
-	// 1
-	glAttachShader(shaderProgram, vertexShader);
-	glAttachShader(shaderProgram, fragmentShader);
-	glLinkProgram(shaderProgram);
-	// 2
-	glAttachShader(shaderProgram2, vertexShader);
-	glAttachShader(shaderProgram2, fragmentShader2);
-	glLinkProgram(shaderProgram);
-
-
-	glDeleteShader(vertexShader);
-	glDeleteShader(fragmentShader);
-
+	// each square is two triangles
 	unsigned int indices[] = {
 		0,1,3,
 		1,2,3
 	};
 
-
-	//glViewport(0, 600, 800, 0);
-
-	unsigned int VBO, VAO, EBO;
-	glGenVertexArrays(1, &VAO);
-	glGenBuffers(1, &VBO);
+	// shared by all objects
+	unsigned int EBO;
 	glGenBuffers(1, &EBO);
 
-	glBindVertexArray(VAO);
+	// Create VBOs and VAOs
+	// Assign indices
+	for (int j = 0; j < num_players; ++j) {
+		players[j].create_shader();
+		glGenVertexArrays(1, &players[j].VAO);
+		glGenBuffers(1, &players[j].VBO);
 
-	glBindBuffer(GL_ARRAY_BUFFER, VBO);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(float)*players[0].length, players[0].vertices, GL_DYNAMIC_DRAW);
+		// 1. bind Vertex Array Object
+		glBindVertexArray(players[j].VAO);
 
+		// 2. copy vertices array into Vertex Buffer Object
+		glBindBuffer(GL_ARRAY_BUFFER, players[j].VBO);
+		glBufferData(GL_ARRAY_BUFFER, sizeof(float)*players[j].length, players[j].vertices, GL_DYNAMIC_DRAW);
 
-	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+		// 3. copy indices array in element buffer
+		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+		glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 
+		// 4. set vertex attributes pointers
+		glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+		glEnableVertexAttribArray(0);
+	}
 
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
-	glEnableVertexAttribArray(0);
 
 	while (!glfwWindowShouldClose(window)) {
 		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT);
 
-		for (int j = 0; j < i.keyboard_count(); ++j) {
-			glBindBuffer(GL_ARRAY_BUFFER, VBO);
+		// Draw square per player
+		for (int j = 0; j < num_players; ++j) {
+			glUseProgram(players[j].shaderProgram);
+			glBindVertexArray(players[j].VAO);
+			glBindBuffer(GL_ARRAY_BUFFER, players[j].VBO);
 			glBufferData(GL_ARRAY_BUFFER, sizeof(float)*players[j].length, players[j].vertices, GL_DYNAMIC_DRAW);
-			glUseProgram(shaderProgram);
-			glBindVertexArray(VAO);
 			glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 		}
 
@@ -199,27 +209,25 @@ int main(int argc, char **argv)
 		glfwPollEvents();
 	}
 
-	glDeleteVertexArrays(1, &VAO);
-	glDeleteBuffers(1, &VBO);
+	for (int j = 0; j < num_players; ++j) {
+		glDeleteVertexArrays(1, &players[j].VAO);
+		glDeleteBuffers(1, &players[j].VBO);
+	}
 	glDeleteBuffers(1, &EBO);
 
 	glfwTerminate();
 
+	// TODO: Figure out how to kill thread immediately
 	play = false;
 
 	return 0;
-}
-
-void framebuffer_size_callback(GLFWwindow *window, int width, int height)
-{
-	glViewport(0, height, width, 0);
 }
 
 void *my_keyboard_handler(void *input)
 {
 	Inputs *i = static_cast<Inputs *>(input);
 	while (play) {
-		for (int j = 0; play && j < players.size(); ++j) {
+		for (int j = 0; j < i->keyboard_count(); ++j) {
 			float o_x = players[j].x;
 			float o_y = players[j].y;
 			if (i->get_key(j, 103))
