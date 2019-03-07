6fd0ad3de7f507ac3d6a495bf4813827c7252408
Peter Ho <ostentatiousitus@gmail.com>
2019-03-06 23:01:34 -0500

    glfwWindowShouldClose()

--- src/input/x86_64asm/test.s
+++ src/input/x86_64asm/test.s
@@ -4,65 +4,72 @@ title: .string "Keyboard Demo - OpenGL"
 main:
 	# rdi,rsi,rdx,rcx,r8,r9
 
-	push	%rbp
-	mov		%rsp,%rbp
-
+	push	%rbp			# save required regs
+	mov	%rsp,%rbp
 	push	%rbx
 	push	%rsi
 	push	%rdi
 	push	%rbp
 
-	call	glfwInit			# no args
+	call	glfwInit		# no args
 
-	mov		$139266,%rdi		# VERSION_MAJOR
-	mov		$3,%rsi
+	mov	$139266,%rdi		# VERSION_MAJOR
+	mov	$3,%rsi			# set to 3
 	call	glfwWindowHint
 
-	mov		$139267,%rdi		# VERSION_MINOR
-	mov		$3,%rsi
+	mov	$139267,%rdi		# VERSION_MINOR
+	mov	$3,%rsi			# set to 3
 	call	glfwWindowHint
 
-	mov		$139272,%rdi		# OPENGL_PROFILE
-	mov		$204801,%rsi		# CORE_PROFILE
+	mov	$139272,%rdi		# OPENGL_PROFILE
+	mov	$204801,%rsi		# CORE_PROFILE
 	call	glfwWindowHint
 
-	mov		$800,%rdi			# WIDTH
-	mov		$600,%rsi			# HEIGHT
-	leaq	title(%rip),%rdx	# "Keyboard Demo - OpenGL"
-	xor		%rcx,%rcx			# NULL
-	xor		%r8,%r8				# NULL
+	mov	$800,%rdi		# width
+	mov	$600,%rsi		# height
+	leaq	title(%rip),%rdx	# title
+	xor	%rcx,%rcx		# null
+	xor	%r8,%r8			# null
 	call	glfwCreateWindow
 
-	mov		%rax,%rbx			# save window pointer
-	mov		%rax,%rdi			# window pointer
+	mov	%rax,%rbx		# save window pointer
+	mov	%rax,%rdi		# window pointer
 	call	glfwMakeContextCurrent
 	
-.forever:						# todo: glfwWindowShouldClose()
+.forever:
 	movss	.color_0_2(%rip),%xmm0
 	movss	.color_0_3(%rip),%xmm1
 	movaps	%xmm1,%xmm2
 	movss	.color_1_0(%rip),%xmm3
 	call	glClearColor
 
-	mov		$16384,%rdi			# GL_COLOR_BUFFER_BIT
+	mov	$16384,%rdi		# GL_COLOR_BUFFER_BIT
 	call	glClear
 
-	mov		%rbx,%rdi			# window pointer
+	mov	%rbx,%rdi		# window pointer
 	call	glfwSwapBuffers
-	jmp		.forever
 
+	call	glfwPollEvents		# no args
 
-	mov		$0,%rax				# return 0
+	mov	%rbx,%rdi
+	call	glfwWindowShouldClose	# window pointer
 
-	pop		%rbx
-	pop		%rsi
-	pop		%rdi
-	pop		%rbp
+	test	%rax,%rax		# if 0
+	je	.forever
 
-	mov		%rbp,%rsp
-	pop		%rbp
-	ret	
+	call	glfwTerminate
 
+	mov	$0,%rax			# return 0
+
+	pop	%rbp			# restore callee-saved
+	pop	%rdi
+	pop	%rsi
+	pop	%rbx
+	mov	%rbp,%rsp
+	pop	%rbp
+	ret
+
+# Floats
 	.align 4
 .color_1_0:
 	.long	1065353216
@@ -75,6 +82,3 @@ main:
 .color_0_2:
 	.long	1045220557
 
-
-.vertexShaderSrc: .asciz "#version 330 core\nlayout (location = 0) in vec3 aPos;\nvoid main()\n{\n   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n}\0"
-.fragmentShaderSrc: .asciz "#version 330 core\nout vec4 FragColor;\nvoid main()\n{\n   FragColor = vec4(%ff, %ff, %ff, 1.0f);\n}\n\0"
